7711d39e611c4e477d1ffa32445fde14d5640f9b
devmarcosv <devmarcosvinicius@gmail.com>
2024-01-12 13:16:10 -0300

    work on auth

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -4,10 +4,7 @@ import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
   <div>
-  <p className='text-3xl font-bold text-indigo-500'>
-    Hello discord clone
-  </p>
-    <Button>Click me</Button>
+ this is protected route
   </div>
   )
 }
