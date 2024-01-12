61462f4763027401cb729a13d1fa8b9ad2d63fd9
devmarcosv <devmarcosvinicius@gmail.com>
2024-01-12 13:42:40 -0300

    work on components

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { UserButton } from '@clerk/nextjs'
+import { ModeToggle } from '@/components/mode-toggle'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <UserButton 
       afterSignOutUrl='/'
     />
+    <ModeToggle />
   </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute='class'
           defaultTheme='dark'
-          forcedTheme='dark'
+          //forcedTheme='dark'
           enableSystem={false}
           storageKey='discord-theme'
         >
