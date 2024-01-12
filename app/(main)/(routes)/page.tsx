5f21ef8b63e483d8d122c0ed6812f4b9a328f2d4
devmarcosv <devmarcosvinicius@gmail.com>
2024-01-12 13:31:10 -0300

    fix some themes and files

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
   <div>
- this is protected route
+    <UserButton 
+      afterSignOutUrl='/'
+    />
   </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/providers/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,9 +18,17 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        {children}
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='dark'
+          forcedTheme='dark'
+          enableSystem={false}
+          storageKey='discord-theme'
+        >
+            {children}
+        </ThemeProvider>
       </body>
     </html>
     </ClerkProvider>
