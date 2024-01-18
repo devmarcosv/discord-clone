3112fc0be80864e146e9e32ef5602175f0e60074
devmarcosv <devmarcosvinicius@gmail.com>
2024-01-17 22:57:19 -0300

    bagunça

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -9,9 +9,9 @@ interface ModalStore {
     onClose: () => void;
 }
 
-export const useModal = create<ModalStore>((set) => ({
+export const useModal = create<ModalStore>((set)=> ({
     type: null,
     isOpen: false,
     onOpen: (type) => set({ isOpen: true, type}),
     onClose: () => set({ type: null, isOpen: false})
-}))
+}));
