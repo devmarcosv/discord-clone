efca016f9baa4262301315124776299d191773a0
devmarcosv <devmarcosvinicius@gmail.com>
2024-01-31 12:32:10 -0300

    work on video calls

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -142,6 +142,42 @@ export const ServerSidebar = async({
                             ))}
                     </div>
                 )}
+                 {!!audioChannels?.length && (
+                    <div className="mb-2">
+                        <ServerSection
+                            sectionType="channels"
+                            channelType={ChannelType.TEXT}
+                            role={role}
+                            label="Voice Channels" />
+                            {audioChannels.map((channel) => (
+                                <ServerChannel 
+                                 key={channel.id}
+                                 channel={channel}
+                                 role={role}
+                                 server={server}
+                                 />
+                            ))}
+                    </div>
+                )}
+                 {!!videoChannels?.length && (
+                    <div className="mb-2">
+                        <ServerSection
+                            sectionType="channels"
+                            channelType={ChannelType.TEXT}
+                            role={role}
+                            label="Video Channels" />
+                            {videoChannels.map((channel) => (
+                                <ServerChannel 
+                                 key={channel.id}
+                                 channel={channel}
+                                 role={role}
+                                 server={server}
+                                 />
+                            ))}
+                    </div>
+                )}
+                
+                
                 
             </ScrollArea>
         </div>
